f5578635468ee22d267ad467132d796f77d48f27
unsorry <m.anshory@gmail.com>
2020-11-15 06:44:14 +0700

    [Commit 2020-11-15]
    - change variable name
    - add opacity legendimg
    - add population in tooltip

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,48 @@
 var map = L.map('map').setView([-7.9,110.45], 10);
 
+// Basemap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'OSM'
 }).addTo(map);
 
 // Control legend
-var legend = L.control({position: 'bottomleft'}), labels;
+var legend = L.control({position: 'bottomleft'}), legenditem;
 
 legend.onAdd = function (map) {
-  this._div = L.DomUtil.create('div', 'legend'), labels = [];
+  this._div = L.DomUtil.create('div', 'legend'), legenditem = [];
   this.update();
   return this._div;
 };
 
 legend.update = function () {
-  this._div.innerHTML = '<h4>Legenda</h4><hr>' + labels.join('<hr>');
+  this._div.innerHTML = '<h4>Legend</h4><hr>' + legenditem.join('<hr>');
 };
 
 legend.addTo(map);
 
 /* GeoJSON Point */
-var titikkabkota = L.geoJson(null, {
+var capitalpoint = L.geoJson(null, {
   onEachFeature: function (feature, layer) {
     if (feature.properties) {
       layer.on({
         mouseover: function (e) {
-          titikkabkota.bindTooltip(feature.properties.KAB_KOTA);
+          capitalpoint.bindTooltip(feature.properties.KAB_KOTA);
         }
       });
     }
   }
 });
-$.getJSON("data/kabupaten_kota_diy_point.geojson", function (data) {
-  titikkabkota.addData(data);
-  map.addLayer(titikkabkota);
+$.getJSON("data/capitalpoint.geojson", function (data) {
+  capitalpoint.addData(data);
+  map.addLayer(capitalpoint);
 });
 
 /* GeoJSON Polyline */
-map.createPane("pane_jalanutama");
-map.getPane("pane_jalanutama").style.zIndex = 401;
+map.createPane("pane_street");
+map.getPane("pane_street").style.zIndex = 401;
 var layerWidth = {"Jalan Arteri":3, "Jalan Kolektor":1};
-var jalanutama = L.geoJson(null, {
-  pane: "pane_jalanutama",
+var street = L.geoJson(null, {
+  pane: "pane_street",
   style: function (feature) {
     return {
       color: "red",
@@ -51,20 +52,20 @@ var jalanutama = L.geoJson(null, {
     };
   }
 });
-$.getJSON("data/jalan_utama_line.geojson", function (data) {
-  jalanutama.addData(data);
-  map.addLayer(jalanutama);
+$.getJSON("data/street.geojson", function (data) {
+  street.addData(data);
+  map.addLayer(street);
 });
 
 /* GeoJSON Polygon */
-map.createPane("pane_adminkecamatan");
-map.getPane("pane_adminkecamatan").style.zIndex = 301;
-var layerColors = {"Tinggi":"#bd0026", "Sedang":"#fd8d3c", "Rendah":"#ffffb2"};
-var adminkecamatan = L.geoJson(null, {
-  pane:"pane_adminkecamatan",
+map.createPane("pane_population");
+map.getPane("pane_population").style.zIndex = 301;
+var layerColors = {"High":"#bd0026", "Middle":"#fd8d3c", "Low":"#ffffb2"};
+var population = L.geoJson(null, {
+  pane:"pane_population",
   style: function (feature) {
     return {
-      fillColor: layerColors[feature.properties.klas_jml],
+      fillColor: layerColors[feature.properties.POPULATION_CLASS],
       fillOpacity: 0.7, 
       color: "black", 
       weight: 1,
@@ -82,10 +83,10 @@ var adminkecamatan = L.geoJson(null, {
           fillColor: "#00FFFF",
           fillOpacity: 1,
         });
-        adminkecamatan.bindTooltip(feature.properties.KECAMATAN + ', ' + feature.properties.KABUPATEN); //Popup
+        population.bindTooltip(feature.properties.KECAMATAN + ', ' + feature.properties.KABUPATEN + '<br>Population ' + parseInt(feature.properties.POPULATION).toLocaleString() + ' people'); //Popup
       },
       mouseout: function (e) {
-        adminkecamatan.resetStyle(e.target);
+        population.resetStyle(e.target);
       },
       click: function (e) {
         map.fitBounds(e.target.getBounds());
@@ -93,61 +94,61 @@ var adminkecamatan = L.geoJson(null, {
     });
   }
 });
-$.getJSON("data/penduduk_kecamatan_diy_polygon.geojson", function (data) {
-  adminkecamatan.addData(data);
-  map.addLayer(adminkecamatan);
+$.getJSON("data/population.geojson", function (data) {
+  population.addData(data);
+  map.addLayer(population);
 });
 
 // Control Layer
 var Layers = {
-  'Ibukota Kabupate/Kota': titikkabkota,
-  'Jalan Utama': jalanutama,
-  'Batas Kecamatan': adminkecamatan,
+  'Capital Point': capitalpoint,
+  'Street': street,
+  'Population': population,
 };
 var layerControl = L.control.layers(null, Layers, {collapsed:false});
 layerControl.addTo(map);
 
 // Legend for each layer
-var legend_titikkabkota = '<img src="assets/img/marker-icon.png" width="10"> Ibukota Kab/Kota',
-legend_jalanutama = '<img src="assets/img/jalan.png" width="150">',
-legend_adminkecamatan = '<img src="assets/img/jumlahpenduduk.png" width="150">';
+var legend_capitalpoint = '<img src="assets/img/marker-icon.png" width="10"> Capital Point',
+legend_street = '<img class="imglegend" src="assets/img/streetlegend.png" width="150">',
+legend_population = '<img class="imglegend" src="assets/img/populationlegend.png" width="150">';
 
 map.on("overlayadd", function(e) {
-  if (e.layer === titikkabkota) {
-    labels.push(legend_titikkabkota);
+  if (e.layer === capitalpoint) {
+    legenditem.push(legend_capitalpoint);
     legend.update();
   }
-  if (e.layer === jalanutama) {
-    labels.push(legend_jalanutama);
+  if (e.layer === street) {
+    legenditem.push(legend_street);
     legend.update();
   }
-  if (e.layer === adminkecamatan) {
-    labels.push(legend_adminkecamatan);
+  if (e.layer === population) {
+    legenditem.push(legend_population);
     legend.update();
   }
 });
 
 map.on("overlayremove", function(e) {
-  if (e.layer === titikkabkota) {
-    for( var i = 0; i < labels.length; i++){
-      if ( labels[i] === legend_titikkabkota) {
-        labels.splice(i, 1);
+  if (e.layer === capitalpoint) {
+    for( var i = 0; i < legenditem.length; i++){
+      if ( legenditem[i] === legend_capitalpoint) {
+        legenditem.splice(i, 1);
       }
     }
     legend.update();
   }
-  if (e.layer === jalanutama) {
-    for( var i = 0; i < labels.length; i++){
-      if ( labels[i] === legend_jalanutama) {
-        labels.splice(i, 1);
+  if (e.layer === street) {
+    for( var i = 0; i < legenditem.length; i++){
+      if ( legenditem[i] === legend_street) {
+        legenditem.splice(i, 1);
       }
     }
     legend.update();
   }
-  if (e.layer === adminkecamatan) {
-    for( var i = 0; i < labels.length; i++){
-      if ( labels[i] === legend_adminkecamatan) {
-        labels.splice(i, 1);
+  if (e.layer === population) {
+    for( var i = 0; i < legenditem.length; i++){
+      if ( legenditem[i] === legend_population) {
+        legenditem.splice(i, 1);
       }
     }
     legend.update();
